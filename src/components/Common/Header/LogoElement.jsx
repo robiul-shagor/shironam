2c8dd27651040ccd286994c52347d141f02814d1
Robiul Shagor <mrhshagor@gmail.com>
2023-07-27 01:44:32 +0600

    code update on sidebar

--- src/components/Common/Header/LogoElement.jsx
+++ src/components/Common/Header/LogoElement.jsx
@@ -1,40 +1,17 @@
 import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import axios from '../../../api/axios';
+import {  useContext } from 'react'
+import { UserContext } from '../../../App';
 
 const LogoElement = () => {
-  const [ loading, setLoading ] = useState( true );
-  const getCategory = async(retryCount = 3, delay = 1000) => {
-    try {
-        await axios.get('/site-settings', {})
-        .then(res => {
-            console.log( res )
-        });
-    } catch (error) {
-        if (retryCount > 0 && error.response?.status === 429) {
-            await new Promise((resolve) => setTimeout(resolve, delay));
-            getCategory(retryCount - 1, delay * 2); 
-        } else {
-            console.log(error);
-            setLoading(false);
-        }
-    } finally {
-        setLoading(false);
-    }
-  };
-
-  useEffect(()=> {
-      //getCategory();
-  }, [])
-
+  const { siteSetting } = useContext(UserContext);
   return (
     <div className="brand-logo text-center">
         <Link 
             to="/"
             className="inline-block max-[575px]:-ml-8"
         >
-            <img src="/assets/media/logo.svg" className="max-[1199px]:w-[12rem] dark:hidden" alt="shironam.com" />
-            <img src="/assets/media/logo-dark.svg" className="max-[1199px]:w-[12rem] hidden dark:inline-block" alt="shironam.com" />
+            <img src={`${siteSetting.base_url}/public/${siteSetting.logo_light}`} className="max-[1199px]:w-[12rem] dark:hidden" alt="shironam.com" width="138" height="52" />
+            <img src={`${siteSetting.base_url}/public/${siteSetting.logo_dark}`} className="max-[1199px]:w-[12rem] hidden dark:inline-block" alt="shironam.com" width="138" height="52" />
         </Link>
     </div>
   )
